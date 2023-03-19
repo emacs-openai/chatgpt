00315664acd46e801aa24a92f4345b9221d1fc77
JenChieh <jcs090218@gmail.com>
2023-03-19 03:17:56 -0700

    start

--- chatgpt.el
+++ chatgpt.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/emacs-openai/chatgpt
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "26.1") (openai "0.1.0"))
+;; Package-Requires: ((emacs "26.1") (openai "0.1.0") (ht "2.0"))
 ;; Keywords: comm openai
 
 ;; This file is not part of GNU Emacs.
@@ -31,13 +31,98 @@
 
 ;;; Code:
 
+(require 'comint)
+
 (require 'openai)
+(require 'ht)
 
 (defgroup chatgpt nil
   "Use ChatGPT inside Emacs."
   :prefix "chatgpt-"
   :group 'comm
   :link '(url-link :tag "Repository" "https://github.com/emacs-openai/chatgpt"))
+
+(defconst chatgpt-buffer-name-format "*ChatGPT: <%s>*"
+  "Name of the buffer to use for the `chatgpt' comint instance.")
+
+(defvar chatgpt-instances (ht-create)
+  "List of instances, each pair is consist of (index . buffer).")
+
+;;
+;;; Util
+
+(defun chatgpt--pop-to-buffer (buffer-or-name)
+  ""
+  (pop-to-buffer buffer-or-name `((display-buffer-in-direction)
+                                  (dedicated . t))))
+
+;;
+;;; Core
+
+(defun chatgpt--live-instances ()
+  "Return a list of live instances."
+  (let ((live-instances))
+    (ht-map (lambda (index buffer)
+              (when (get-buffer buffer)
+                (push buffer live-instances)))
+            chatgpt-instances)
+    (reverse live-instances)))
+
+(defun chatgpt--shown-instances ()
+  "Return a list of live instances that are displayed on the screen."
+  (let ((live-instances (chatgpt--live-instances))
+        (shown-instances))
+    (dolist (instance shown-instances)
+      (when (get-buffer-window instance)
+        (push instance shown-instances)))
+    (reverse shown-instances)))
+
+(defun chatgpt--new-index ()
+  "Find killed instance before giving new index."
+  (let ((target))
+    (cl-some (lambda (index)
+               (let ((buffer (ht-get chatgpt-instances index)))
+                 (unless (get-buffer buffer)  ; if buffer is killed
+                   (setq target index)
+                   t)))
+             (ht-keys chatgpt-instances))
+    (unless target                                ; No killed instance?
+      (setq target (ht-size chatgpt-instances)))  ; Create a new one!
+    target))
+
+;;;###autoload
+(define-derived-mode chatgpt-mode comint-mode "ChatGPT"
+  "Major mode for `chatgpt-mode'.
+
+\\<chatgpt-mode-map>"
+  (setq comint-prompt-regexp cassandra-prompt-regexp
+        comint-prompt-read-only t))
+
+;;;###autoload
+(defun chatgpt-new ()
+  "Run a new instance of ChatGPT."
+  (interactive)
+  (let* ((new-index       (chatgpt--new-index))
+         (new-buffer-name (format chatgpt-buffer-name-format new-index)))
+    (when (get-buffer new-buffer-name)
+      (user-error "Internal Error: creating instance that already exists"))
+    (ht-set chatgpt-instances new-index (get-buffer-create new-buffer-name))
+    (with-current-buffer new-buffer-name
+      (chatgpt-mode 1))
+    (chatgpt--pop-to-buffer new-buffer-name)))
+
+;;;###autoload
+(defun chatgpt ()
+  "Start ChatGPT with existing instance, else create a new instance."
+  (interactive)
+  (let ((live-instances  (chatgpt--live-instances))
+        (shown-instances (chatgpt--shown-instances)))
+    (cond (shown-instances
+           (chatgpt--pop-to-buffer (nth 0 shown-instances)))
+          (live-instances
+           (chatgpt--pop-to-buffer (nth 0 shown-instances)))
+          (t
+           (chatgpt-new)))))
 
 (provide 'chatgpt)
 ;;; chatgpt.el ends here
